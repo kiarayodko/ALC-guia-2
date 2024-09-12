47e320c45d802f3ed6d5897bdac89272f014728f
kiarayodko <145400769+kiarayodko@users.noreply.github.com>
2024-09-12 18:39:04 -0300

    Add files via upload

---
+++ elim_gaussiana.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Eliminacion Gausianna
+"""
+import numpy as np
+
+def elim_gaussiana(A):
+    cant_op = 0
+    m=A.shape[0]
+    n=A.shape[1]
+    Ac = A.copy()
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return
+    
+    ## desde aqui -- CODIGO A COMPLETAR
+    L= np.eye(n)
+
+    for i in range (n):
+        if Ac[i, i, ]==0:
+            #deberia hacer una permutacion
+            print (f"pivote {i} es nulo")
+            return
+        
+        
+        for j in range (i+1, n):
+            factor = Ac[j,i]/Ac[i,i]
+            L[j,i]=factor #guardamos el factor en la matriz L
+            #cant_op #...
+            Ac[j, i:]=Ac[j,i:]-factor*Ac[i,i:]
+            print (f"Nueva matriz, despues del paso ({i}, {j})")
+            print (L)
+            
+         
+    ## hasta aqui
+            
+    #L = np.tril(Ac,-1) + np.eye(A.shape[0]) 
+    U = np.triu(Ac)
+    
+    return L, U, cant_op
+
+
+def main():
+    n = 7
+    B = np.eye(n) - np.tril(np.ones((n,n)),-1) 
+    B[:n,n-1] = 1
+    print('Matriz B \n', B)
+    
+    L,U,cant_oper = elim_gaussiana(B)
+    
+    print('Matriz L \n', L)
+    print('Matriz U \n', U)
+    print('Cantidad de operaciones: ', cant_oper)
+    print('B=LU? ' , 'Si!' if np.allclose(np.linalg.norm(B - L@U, 1), 0) else 'No!')
+    print('Norma infinito de U: ', np.max(np.sum(np.abs(U), axis=1)) )
+
+if __name__ == "__main__":
+    main()
+    
+    
